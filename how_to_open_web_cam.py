558e98967c6d3018e2975da467d7ba784f8b3463
Shadman Shakib <shadman434@gmail.com>
2025-01-10 20:19:17 -0600

    video output in gray color

--- how_to_open_web_cam.py
+++ how_to_open_web_cam.py
@@ -3,8 +3,9 @@ import cv2
 capture = cv2.VideoCapture(0);
 while(True):
     ret, frame = capture.read()
-
-    cv2.imshow('frame', frame)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.imshow('frame', frame)
+    cv2.imshow('gray_frame', gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 capture.release()
