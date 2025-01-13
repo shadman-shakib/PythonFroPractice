fcd52022fe250bf38abc71cd833da94af0c51cdf
Md Shadman Shakib <shadman434@gmail.com>
2025-01-12 22:31:54 -0600

    how to save video

--- how_to_open_web_cam.py
+++ how_to_open_web_cam.py
@@ -1,12 +1,21 @@
 import cv2
 
-capture = cv2.VideoCapture(0);
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output_video.avi', fourcc, 20.0, (640, 480))
+capture = cv2.VideoCapture(0)
 while(True):
     ret, frame = capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('frame', frame)
-    cv2.imshow('gray_frame', gray)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    if ret:
+        out.write(frame)
+        #gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cv2.imshow('frame', frame)
+        #cv2.imshow('frame', gray)
+        #cv2.imshow('frame', gray)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    else:
         break
 capture.release()
+out.release()
 cv2.destroyAllWindows()
